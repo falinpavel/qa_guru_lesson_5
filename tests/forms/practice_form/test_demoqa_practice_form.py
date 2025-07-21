9def387a6df7bacebb498b5a2ce40602d6df2f7e
Falin Pavel <falinpavel96@gmail.com>
2025-07-21 11:04:57 +0300

    remove browser.element/browser.all -> s/ss

--- tests/forms/practice_form/test_demoqa_practice_form.py
+++ tests/forms/practice_form/test_demoqa_practice_form.py
@@ -1,6 +1,7 @@
 import os
 from selene import browser, have, be, command
 from selene.core.condition import Condition
+from selene.support.shared.jquery_style import s, ss
 
 
 """
@@ -35,77 +36,77 @@ def test_successful_filling_students_registration_form():
             have.text('Practice Form'), have.text('Student Registration Form')
         )
     )
-    browser.element('#userName-wrapper').should(have.text('Name'))
-    browser.element('#firstName').should(be.blank).type('Ivan').should(have.value('Ivan'))
-    browser.element('#lastName').should(be.blank).type('Ivanov').should(have.value('Ivanov'))
-    browser.element('#userEmail-wrapper').should(have.text('Email'))
-    browser.element('#userEmail').should(be.blank).type('test@example.com').should(have.value('test@example.com'))
-    browser.element('#genterWrapper').should(have.text('Gender'))
-    gender_elements = browser.all('label[class="custom-control-label"]')
-    gender_elements[0].should(have.text('Male')).click()
-    gender_elements[1].should(have.text('Female')).click()
-    gender_elements[2].should(have.text('Other')).click()
-    browser.element('#userNumber-label').should(
+    s('#userName-wrapper').should(have.text('Name'))
+    s('#firstName').should(be.blank).type('Ivan').should(have.value('Ivan'))
+    s('#lastName').should(be.blank).type('Ivanov').should(have.value('Ivanov'))
+    s('#userEmail-wrapper').should(have.text('Email'))
+    s('#userEmail').should(be.blank).type('test@example.com').should(have.value('test@example.com'))
+    s('#genterWrapper').should(have.text('Gender'))
+    genders = ss('label[class="custom-control-label"]')
+    genders[0].should(have.text('Male')).click()
+    genders[1].should(have.text('Female')).click()
+    genders[2].should(have.text('Other')).click()
+    s('#userNumber-label').should(
         Condition.by_and(
             have.text('Mobile'), have.text('(10 Digits)')
         )
     )
-    browser.element('#userNumber').should(be.blank).type('88002556535')
-    browser.element('#dateOfBirth-label').should(have.text('Date of Birth'))
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('option[value="4"]').with_(timeout=browser.config.timeout*2).should(be.visible).click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('option[value="1996"]').with_(timeout=browser.config.timeout*2).should(be.visible).click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('div[aria-label="Choose Thursday, May 23rd, 1996"]').click()
-    browser.element('#subjectsWrapper').should(have.text('Subjects'))
-    subjects_field = browser.element('#subjectsInput')
-    subjects_field.type('Computer Science').should(have.value('Computer Science')).press_enter()
-    subjects_field.type('Maths').should(have.value('Maths')).press_enter()
-    subjects_field.type('Physics').should(have.value('Physics')).press_enter()
-    subjects_field.type('Bio').should(have.value('Bio')).press_enter()
+    s('#userNumber').should(be.blank).type('88002556535')
+    s('#dateOfBirth-label').should(have.text('Date of Birth'))
+    s('#dateOfBirthInput').click()
+    s('.react-datepicker__month-select').click()
+    s('option[value="4"]').with_(timeout=browser.config.timeout*2).should(be.visible).click()
+    s('.react-datepicker__month-select').click()
+    s('.react-datepicker__year-select').click()
+    s('option[value="1996"]').with_(timeout=browser.config.timeout*2).should(be.visible).click()
+    s('.react-datepicker__year-select').click()
+    s('div[aria-label="Choose Thursday, May 23rd, 1996"]').click()
+    s('#subjectsWrapper').should(have.text('Subjects'))
+    subjects = browser.element('#subjectsInput')
+    subjects.type('Computer Science').should(have.value('Computer Science')).press_enter()
+    subjects.type('Maths').should(have.value('Maths')).press_enter()
+    subjects.type('Physics').should(have.value('Physics')).press_enter()
+    subjects.type('Bio').should(have.value('Bio')).press_enter()
     browser.element('#hobbiesWrapper').should(have.text('Hobbies'))
-    hobbies_elements = browser.all('label[class="custom-control-label"]')
-    hobbies_elements[3].should(have.text('Sports')).click()
-    hobbies_elements[4].should(have.text('Reading')).click()
-    hobbies_elements[5].should(have.text('Music')).click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath(file_path))
-    browser.element('#currentAddress-wrapper').should(have.text('Current Address'))
-    browser.element('#currentAddress').should(be.blank).type('Moscow')
-    browser.element('#stateCity-wrapper').perform(command.js.scroll_into_view)
-    browser.element('#stateCity-wrapper').should(have.text('State and City'))
-    browser.element('#state').click()
-    browser.element('#react-select-3-option-1').click()
-    browser.element('#city').click()
-    browser.element('#react-select-4-option-1').click()
-    browser.element('#submit').click()
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    hobbies = ss('label[class="custom-control-label"]')
+    hobbies[3].should(have.text('Sports')).click()
+    hobbies[4].should(have.text('Reading')).click()
+    hobbies[5].should(have.text('Music')).click()
+    s('#uploadPicture').send_keys(os.path.abspath(file_path))
+    s('#currentAddress-wrapper').should(have.text('Current Address'))
+    s('#currentAddress').should(be.blank).type('Moscow')
+    s('#stateCity-wrapper').perform(command.js.scroll_into_view)
+    s('#stateCity-wrapper').should(have.text('State and City'))
+    s('#state').click()
+    s('#react-select-3-option-1').click()
+    s('#city').click()
+    s('#react-select-4-option-1').click()
+    s('#submit').click()
+    s('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     """
     Далее по коду идут проверки корректного заполнения формы,
      матчим ключи и значения построчно
     """
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Student Name')).all('td').second.should(have.text('Ivan Ivanov'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Student Email')).all('td').second.should(have.text('test@example.com'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Gender')).all('td').second.should(have.text('Other'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Mobile')).all('td').second.should(have.text('8800255653'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Date of Birth')).all('td').second.should(have.text('23 May,1996'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Subjects')).all('td').second.should(have.text('Computer Science, Maths, Physics, Biology'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Hobbies')).all('td').second.should(have.text('Sports, Reading, Music'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Picture')).all('td').second.should(have.text('file.txt'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('Address')).all('td').second.should(have.text('Moscow'))
-    browser.all('table.table-dark tbody tr').element_by(
-        have.text('State and City')).all('td').second.should(have.text('Uttar Pradesh Lucknow'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Student Name')).ss('td').second.should(have.text('Ivan Ivanov'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Student Email')).ss('td').second.should(have.text('test@example.com'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Gender')).ss('td').second.should(have.text('Other'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Mobile')).ss('td').second.should(have.text('8800255653'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Date of Birth')).ss('td').second.should(have.text('23 May,1996'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Subjects')).ss('td').second.should(have.text('Computer Science, Maths, Physics, Biology'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Hobbies')).ss('td').second.should(have.text('Sports, Reading, Music'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Picture')).ss('td').second.should(have.text('file.txt'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('Address')).ss('td').second.should(have.text('Moscow'))
+    ss('table.table-dark tbody tr').element_by(
+        have.text('State and City')).ss('td').second.should(have.text('Uttar Pradesh Lucknow'))
 
 
 def test_with_empty_fields():
@@ -116,7 +117,6 @@ def test_with_empty_fields():
     4. Проверить что модальное окно "Thanks for submitting the form" не появляется,
          т.е. прошла проверка на обязательность полей
     """
-    browser.open('/automation-practice-form')
-    browser.element('#submit').perform(command.js.scroll_into_view)
-    browser.element('#submit').click()
-    browser.element('#example-modal-sizes-title-lg').should(be.absent)
+    s('#submit').perform(command.js.scroll_into_view)
+    s('#submit').click()
+    s('#example-modal-sizes-title-lg').should(be.absent)
