b8fac318edcfaf647d2b6e00d483236b078dcd13
Falin Pavel <falinpavel96@gmail.com>
2025-07-21 14:18:58 +0300

    edit conftest.py

--- conftest.py
+++ conftest.py
@@ -11,5 +11,6 @@ def browser_options():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     driver_options.add_argument('--start-maximized')
+    # driver_options.add_argument('--headless')
     browser.config.base_url = 'https://demoqa.com'
     return driver_options
