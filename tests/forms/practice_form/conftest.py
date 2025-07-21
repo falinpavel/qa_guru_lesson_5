6cf350e55a76d8e9af865f6a79b6b9f129d7192d
Falin Pavel <falinpavel96@gmail.com>
2025-07-21 14:20:10 +0300

    +1

--- tests/forms/practice_form/conftest.py
+++ tests/forms/practice_form/conftest.py
@@ -2,10 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(
-    scope="function",
-    autouse=True
-)
+@pytest.fixture(scope="function",autouse=True)
 def browser_open_and_quit(browser_options):
     browser.config.driver_options = browser_options
     browser.open('/automation-practice-form')
